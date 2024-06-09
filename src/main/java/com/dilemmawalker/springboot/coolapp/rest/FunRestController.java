5212b3b80c638daf802126b927bf8c53da162f8c
Yash Handa <yashhanda500@gmail.com>
2024-06-09 18:22:10 +0530

    first commit

---
+++ src/main/java/com/dilemmawalker/springboot/coolapp/rest/FunRestController.java
@@ -0,0 +1,27 @@
+package com.dilemmawalker.springboot.coolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${team.name}") // picking up from application.properties within Maven.
+    private String teamName;
+
+    @GetMapping("/")
+    public String sayHello(){
+        return teamName;
+    }
+
+    @GetMapping("/working")
+    public String working(){
+        return "I am not working";
+    }
+
+    @GetMapping("/hardoop")
+    public String hardoop(){
+        return "I am a hardoop";
+    }
+}
---
+++ src/test/java/com/dilemmawalker/springboot/coolapp/CoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilemmawalker.springboot.coolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
